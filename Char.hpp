7fec882a7e2a959518dc3245739a818dfba22b3a
CodeMouse179 <codemouse179@gmail.com>
2023-04-23 17:48:53 +0300

    update Char.hpp, Example.cpp

--- Char.hpp
+++ Char.hpp
@@ -1,3 +1,30 @@
 #ifndef SYSTEM_CHAR_HPP
 #define SYSTEM_CHAR_HPP
+
+#include <limits>
+
+namespace System
+{
+    template<typename T>
+    class Char
+    {
+    private:
+        //static class
+        Char()
+        {
+        }
+
+    public:
+        static const T MaxValue = std::numeric_limits<T>::max();
+
+        static const T MinValue = std::numeric_limits<T>::min();
+
+    public:
+    };
+
+    typedef System::Char<char> char_t;
+    typedef System::Char<char16_t> char16;
+    typedef System::Char<char32_t> char32;
+}
+
 #endif
--- Example.cpp
+++ Example.cpp
@@ -1,6 +1,16 @@
 #include "Char.hpp"
+#include <iostream>
 
 int main()
 {
+    std::cout << (int)System::char_t::MinValue << "\n";
+    std::cout << (int)System::char_t::MaxValue << "\n";
+
+    std::cout << System::char16::MinValue << "\n";
+    std::cout << System::char16::MaxValue << "\n";
+
+    std::cout << System::char32::MinValue << "\n";
+    std::cout << System::char32::MaxValue << "\n";
+
     return 0;
 }
